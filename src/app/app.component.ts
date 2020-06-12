d21b2b423b0161b71c9d34a106500e717e96ab90
AmyTroutman <amyrtroutman@gmail.com>
2020-06-11 20:14:17 -0500

    TodoService done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import {NgbModal, ModalDismissReasons} from '@ng-bootstrap/ng-bootstrap';
-import { ModalComponent} from './modal/modal.component';
 import { ITodo } from './itodo';
+import { TodoService } from './services/todo.service';
 
 @Component({
   selector: 'app-root',
@@ -9,47 +8,14 @@ import { ITodo } from './itodo';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
-  title = 'Todos';
-  todoList: ITodo [] = [];
-  todoTitle: string;
-  todoId = 0;
   ngOnInit() {
-    this.todoTitle = '';
-    this.todoList = [
+    this.todoService.todoTitle = '';
+    this.todoService.todoList = [
       // example of how to make an item in todo list
       {id: 1, title: 'Install Angular CLI', isDone: false },
 
     ];
   }
 
-  constructor(private modalService: NgbModal) {}
-
-  addTodo(): void {
-    this.todoList.push({
-      id: this.todoId,
-      title: this.todoTitle,
-      isDone: false
-    });
-
-    // resets our todoTitle variable to an empty string
-    this.todoTitle = '';
-    this.todoId++;
-  }
-
-  async deleteTodo(todo: any) {
-    const modal = this.modalService.open(ModalComponent);
-    const modalComponent = modal.componentInstance;
-    modalComponent.modalInstance = modal;
-
-    try {
-      const theResult = await modal.result;
-      if (theResult === 'yes') {
-        const index = this.todoList.findIndex(todoItem => todoItem === todo);
-        this.todoList.splice(index, 1);
-      }
-    } catch (ex) {
-      console.log(ex);
-    }
-  }
-
+  constructor(private todoService: TodoService) {}
 }
--- src/app/services/todo.service.ts
+++ src/app/services/todo.service.ts
@@ -1,9 +1,48 @@
 import { Injectable } from '@angular/core';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { ModalComponent } from '../modal/modal.component';
+import { ITodo } from '../itodo';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TodoService {
 
-  constructor() { }
+  constructor(private modalService: NgbModal) {
+  }
+  // tslint:disable-next-line: no-trailing-whitespace
+  title = 'Todos';
+  todoList: ITodo [] = [];
+  todoTitle: string;
+  todoId = 0;
+
+  addTodo(): void {
+    this.todoList.push({
+      id: this.todoId,
+      title: this.todoTitle,
+      isDone: false
+    });
+
+    // resets our todoTitle variable to an empty string
+    this.todoTitle = '';
+    this.todoId++;
+  }
+
+  async deleteTodo(todo: any) {
+    const modal = this.modalService.open(ModalComponent);
+    const modalComponent = modal.componentInstance;
+    modalComponent.modalInstance = modal;
+
+    try {
+      const theResult = await modal.result;
+      if (theResult === 'yes') {
+        const index = this.todoList.findIndex(todoItem => todoItem === todo);
+        this.todoList.splice(index, 1);
+      }
+    } catch (ex) {
+      console.log(ex);
+    }
+  }
+
+  getTodos(){}
 }
